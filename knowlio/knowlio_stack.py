5188bce14785f96a05bb130b2c8c08c1d437c49a
Vishal Nyalchand <nyalchan@amazon.com>
2024-12-24 13:46:41 -0800

    Initial commit with project files

--- knowlio/knowlio_stack.py
+++ knowlio/knowlio_stack.py
@@ -1,19 +1,17 @@
 from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
+    Stack
 )
 from constructs import Construct
+import os
+from aws_cdk import aws_lambda as _lambda
+from aws_cdk import aws_apigateway as apigateway
 
 class KnowlioStack(Stack):
+    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
-        super().__init__(scope, construct_id, **kwargs)
-
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "KnowlioQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        lambda_function = _lambda.Function(self, "MyLambdaFunction",
+            runtime=_lambda.Runtime.PYTHON_3_8,
+            handler="lambda_function.handler",
+            code=_lambda.Code.from_asset(os.path.join(os.path.dirname(__file__), "src"))
+        )
---
+++ knowlio/src/lambda_function.py
@@ -0,0 +1,6 @@
+def handler(event, context):
+    # Sample Lambda function logic
+    return {
+        "statusCode": 200,
+        "body": "Hello from Lambda!"
+    }
